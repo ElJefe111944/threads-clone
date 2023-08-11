ed981f8737d5ca8c3740d2ae6c1ee0d80232eb2c
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-11 11:48:25 +0100

    Sign out button added to nav and styled

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,3 +1,4 @@
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,6 +11,17 @@ export default function TopBar() {
                     Threads
                 </p>
             </Link>
+            <div className="flex items-center gap-1">
+                <div className="block md:hidden">
+                    <SignedIn>
+                        <SignOutButton>
+                            <div className="flex cursor-pointer">
+                                <Image src='/assets/logout.svg' alt="logout" width={24} height={24} />
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+                </div>
+            </div>
         </nav>
     )
   }
