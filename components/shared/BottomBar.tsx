8508c62a208f1e699aa9a938e450cbbef65d045b
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-12 10:32:00 +0100

    BottomBar links and styling added along with isActive functionality. BottomBar to be displayed as a nav on tablet and mobile

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -1,3 +1,45 @@
+'use client'
+
+import Link from "next/link";
+import { sidebarLinks } from '../../constants/index'
+import Image from "next/image";
+import { usePathname } from 'next/navigation'
+
+interface SideBarLink {
+  imgURL: string;
+  route: string;
+  label: string;
+}
+
 export default function BottomBar() {
-    return <h1>Bottom Bar</h1>
-  }
+
+  const pathname = usePathname();
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((item: SideBarLink) => {
+
+          // check if link is active
+          const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
+
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={`bottombar_link ${isActive && 'bg-primary-500'}`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">{item.label.split(/\s+./)[0]}</p>
+            </Link>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
