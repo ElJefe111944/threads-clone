c9035a7288423b0148542378d345e86f16627829
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-14 18:38:51 +0100

    User validation added using zod resolver

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,8 +1,34 @@
 'use client'
 
-const AccountProfile = () => {
-  return (
-    <div>AccountProfile</div>
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import {
+  Form
+} from "@/components/ui/form"
+
+interface UserProps {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string
+    };
+    btnTitle: string;
+}
+
+const AccountProfile = ({ user, btnTitle }): UserProps => {
+  
+  const form = useForm({
+    resolver
+  });
+  
+  return (    
+    <Form>
+
+    </Form>
   )
 }
 
