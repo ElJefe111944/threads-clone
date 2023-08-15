cd41c61044b0af6d1f3af30eb45a05771b5b3c5e
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-15 11:18:28 +0100

    Onchange funcftionality added to the handleImage function inside AccountProfile to update the profile image when the user uploads a new image in the onboarding page

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -17,7 +17,7 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { UserValidation } from "@/lib/validations/user";
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 
 interface UserProps {
   user: {
@@ -33,6 +33,8 @@ interface UserProps {
 
 const AccountProfile = ({ user, btnTitle }: UserProps) => {
 
+  const [files, setFiles] = useState<File[]>([]);
+
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
@@ -45,6 +47,24 @@ const AccountProfile = ({ user, btnTitle }: UserProps) => {
 
   const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
     e.preventDefault();
+
+    const fileReader = new FileReader();
+
+    if(e.target.files && e.target.files.length > 0){
+      const file = e.target.files[0];
+
+      setFiles(Array.from(e.target.files));
+
+      if(!file.type.includes('image')) return;
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || '';
+
+        fieldChange(imageDataUrl);
+
+      };
+      fileReader.readAsDataURL(file);
+    }
   };
 
   function onSubmit(values: z.infer<typeof UserValidation>) {
