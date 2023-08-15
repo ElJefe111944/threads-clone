99f92ab5e2794ee59f9c5ce9c0140bf3a9a33477
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-15 10:30:21 +0100

    Form input fields and textarea added to onboarding page along with the styling

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -14,7 +14,10 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
 import { UserValidation } from "@/lib/validations/user";
+import Image from "next/image";
+import { ChangeEvent } from "react";
 
 interface UserProps {
   user: {
@@ -40,32 +43,106 @@ const AccountProfile = ({ user, btnTitle }): UserProps => {
     }
   });
 
+  const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
+    e.preventDefault();
+  };
+
   function onSubmit(values: z.infer<typeof UserValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values)
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-start gap-10">
+        {/* profile photo */}
+        <FormField
+          control={form.control}
+          name="profile_photo"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-4">
+              <FormLabel className="account-form_image-label">
+                {field.value ? (
+                  <Image
+                    src={field.value}
+                    width={96}
+                    height={96}
+                    alt="profile photo"
+                    priority
+                    className="rounded-full object-contain"
+                  />
+                ) : (
+                  <Image
+                    src='/assets/profile.svg'
+                    width={24}
+                    height={24}
+                    alt="profile photo"
+                    className="object-contain"
+                  />
+                )}
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <Input
+                  type="file"
+                  accept="image/*"
+                  placeholder="Upload a photo"
+                  className="account-form_image-input"
+                  onChange={(e) => handleImage(e, field.onChange)}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        {/* name */}
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="flex gap-3 w-full flex-col">
+              <FormLabel className="text-base-semibold text-light-2">Name:</FormLabel>
+              <FormControl>
+                <Input
+                  className="account-form_input no-focus"
+                  type="text"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        {/* username */}
         <FormField
           control={form.control}
           name="username"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
+            <FormItem className="flex gap-3 w-full flex-col">
+            <FormLabel className="text-base-semibold text-light-2">Username:</FormLabel>
+            <FormControl>
+                <Input
+                  className="account-form_input no-focus"
+                    type="text"
+                  {...field}
+                />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        {/*  */}
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem className="flex gap-3 w-full flex-col">
+            <FormLabel className="text-base-semibold text-light-2">Bio:</FormLabel>
+            <FormControl>
+                <Textarea
+                  className="account-form_input no-focus"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button className="bg-primary-500" type="submit">Submit</Button>
       </form>
     </Form>
   )
