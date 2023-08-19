5cc3d1978e907e292c859ce933889c7d4094137b
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-19 12:15:53 +0100

    ThreadValidation & Comment validation added to Threads in validation folder

---
+++ lib/validations/thread.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const ThreadValidation = z.object({
+    thread: z.string().nonempty().min(3, { message: 'Minimum of 3 characters' }),
+    acountId: z.string(),
+});
+
+export const CommentValidation = z.object({
+    thread: z.string().nonempty().min(3, { message: 'Minimum of 3 characters' }),
+    acountId: z.string(),
+});
+
+
