e74005c2f771d94b08f0de59f988aba650b5837c
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-20 11:12:52 +0100

    Form added to PostThread component

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -46,5 +46,30 @@ export default function PostThread({ userId }: { userId: string }) {
         }
     });
 
-    return <h1 className="text-white">Post Thread Form</h1>
+    const onSubmit = () => {
+        
+    };
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-start gap-10">
+                <FormField
+                    control={form.control}
+                    name="thread"
+                    render={({ field }) => (
+                        <FormItem className="flex gap-3 w-full flex-col">
+                            <FormLabel className="text-base-semibold text-light-2">Content:</FormLabel>
+                            <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                                <Textarea
+                                    rows={15}
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </form>
+        </Form>
+    )
 }
