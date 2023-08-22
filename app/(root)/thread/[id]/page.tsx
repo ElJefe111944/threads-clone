33156ffcf91660a43be88b85cb9f13e99c0e7576
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-22 12:53:46 +0100

    Thread details page created

---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Page({ params }: { params: { id : string }}) {
+    return (
+        <section className="relative">
+            <div>
+                
+            </div>
+        </section>
+    )
+  }
