62e1105ad9eb483c2af6e21e4c50b8a2fe63955a
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-22 12:28:41 +0100

    Image, Content & Profile name added to ThreadCard and styled

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 
 
 interface Props {
@@ -38,8 +40,26 @@ export default function ThreadCard({
 
 
   return (
-    <>
-
-    </>
+      <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+          <div className="flex items-start justify-between">
+              <div className="flex w-full flex-1 flex-row gap-4">
+                  <div className="flex flex-col items-center">
+                      <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+                          <Image src={author.image} alt={author.name} fill className="cursor-pointer rounded-full" />
+                      </Link>
+                      <div className="thread-card_bar" />
+                  </div>
+                  <div className="flex w-full flex-col">
+                      <Link className="w-fit" href={`/profile/${author.id}`}><h4 className="cursor-pointer text-base-semibold text-light-1">{author.name}</h4></Link>
+                      <p className="mt-2 text-small-regular text-light-2">{content}</p>
+                      <div className="mt-5 flex flex-col gap-3">
+                        <div className="flex gap-3.5">
+                            <Image src='./assets/heart-gray.svg' alt="heart" width={24} height={24} className="cursor-pointer object-contain" />
+                        </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </article>
   );
 }
