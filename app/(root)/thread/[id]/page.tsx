7908a6a880777f8ef4fabe198d706205c3f62678
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-23 11:32:08 +0100

    ThreadCard added to individual thread page

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,9 +1,30 @@
+import ThreadCard from "@/components/cards/ThreadCard"
+import { currentUser } from "@clerk/nextjs";
+import { fetchUser } from "@/lib/actions/user.actions";
 
-export default function Page({ params }: { params: { id : string }}) {
+export async function Page ({ params }: { params: { id : string }}) {
+
+    if(!params.id) return null;
+
+    const user = await currentUser();
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
     return (
         <section className="relative">
             <div>
-                
+            <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id || " "}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
             </div>
         </section>
     )
