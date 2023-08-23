840fb874c3ea58d29fbece79589c88fba795e508
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-23 12:01:19 +0100

    function in Thread details page exported as default

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,8 +1,10 @@
 import ThreadCard from "@/components/cards/ThreadCard"
 import { currentUser } from "@clerk/nextjs";
 import { fetchUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import { fetchThreadById } from "@/lib/actions/thread.actions";
 
-export async function Page ({ params }: { params: { id : string }}) {
+async function Page ({ params }: { params: { id : string }}) {
 
     if(!params.id) return null;
 
@@ -10,22 +12,27 @@ export async function Page ({ params }: { params: { id : string }}) {
     if(!user) return null;
 
     const userInfo = await fetchUser(user.id);
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
+    const thread = await fetchThreadById(params.id);
 
     return (
         <section className="relative">
             <div>
             <ThreadCard
-                key={post._id}
-                id={post._id}
+                key={thread._id}
+                id={thread._id}
                 currentUserId={user?.id || " "}
-                parentId={post.parentId}
-                content={post.text}
-                author={post.author}
-                community={post.community}
-                createdAt={post.createdAt}
-                comments={post.children}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
               />
             </div>
         </section>
     )
   }
+
+  export default Page;
