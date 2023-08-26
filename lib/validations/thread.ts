f0a0a1b4f77d6d89cd4656ce9f02ee82de11f8c4
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-26 16:11:50 +0100

    AccountId removed from CommentValidation

--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -7,7 +7,6 @@ export const ThreadValidation = z.object({
 
 export const CommentValidation = z.object({
     thread: z.string().nonempty().min(3, { message: 'Minimum of 3 characters' }),
-    accountId: z.string(),
 });
 
 
