197074a2219d8e0d9a2bf07b1fd595088e70daaf
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-27 11:52:37 +0100

    Tab headers added to Profile page and styled

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image";
 import { currentUser } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 import { fetchUser } from "@/lib/actions/user.actions";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { profileTabs } from "@/constants";
+
 import ProfileHeader from "@/components/shared/ProfileHeader";
 
 
@@ -16,7 +20,7 @@ async function page({ params }: { params: { id: string } }) {
 
   return (
     <section>
-      <ProfileHeader 
+      <ProfileHeader
         accountId={userInfo.id}
         authUserId={user.id}
         name={userInfo.name}
@@ -24,6 +28,28 @@ async function page({ params }: { params: { id: string } }) {
         imgUrl={userInfo.image}
         bio={userInfo.bio}
       />
+
+      <div className="mt-9">
+        <Tabs defaultValue="threads" className="w-full">
+          <TabsList className="tab">
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className="tab">
+                <Image src={tab.icon} alt={tab.label} width={24} height={24} className="object-contain" />
+                <p className="max-sm:hidden">{tab.label}</p>
+
+                {tab.label === 'Threads' && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo?.threads?.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          <TabsContent className="text-white" value="threads">Threads</TabsContent>
+          <TabsContent className="text-white" value="replies">Replies</TabsContent>
+          <TabsContent className="text-white" value="tagged">Tagged</TabsContent>
+        </Tabs>
+      </div>
     </section>
   )
 }
