2699b55984c4b9a190328ead2d1e612ca4594f4e
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-27 10:58:29 +0100

    ProfileHeader component created and rendered in profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,9 +1,22 @@
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+import { fetchUser } from "@/lib/actions/user.actions";
+import ProfileHeader from "@/components/shared/ProfileHeader";
 
 
-async function page() {
+async function page({ params }: { params: { id: string } }) {
+
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(params.id);
+
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
   return (
     <section>
-        Profile
+      <ProfileHeader />
     </section>
   )
 }
