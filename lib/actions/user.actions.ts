48440aea433d84cd250088854df2c79da2b4d40d
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-30 11:47:40 +0100

    FetchUsers function created and search functionality added by name & username. Sort functionality to be implemented

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
     userId: string;
@@ -86,5 +87,42 @@ export async function fetchUserPosts(userId: string) {
 
     } catch (error) {
         throw new Error(`Failed to fetch user post: ${error}`);
+    }
+};
+
+export async function fetchUsers({
+    userId,
+    searchString = "",
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = 'desc'
+} : {
+    userId: string,
+    searchString?: string,
+    pageNumber?: number,
+    pageSize?: number,
+    sortBy?: SortOrder,
+}){
+    try {
+
+        connectToDB();
+
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const regex = new RegExp(searchString, 'i');
+
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId },
+        };
+
+        if(searchString.trim() !== ''){
+            query.$or = [
+                { username: { $regrex: regex } },
+                { name: { $regrex: regex } },
+            ]
+        };
+
+    }catch (error){
+        console.log(error);
     }
 };
