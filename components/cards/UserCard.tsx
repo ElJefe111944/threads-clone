79d8e43f0d7121d25d8219ea865e0f9de552edf0
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-09-01 08:55:33 +0100

    Button added to UserCard and styled

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import Image from "next/image";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
 
 interface UserCardProps {
     id: string;
@@ -9,11 +13,23 @@ interface UserCardProps {
 }
 
 function UserCard({ id, name, username, imgUrl, personType }:UserCardProps) {
+
+    const router = useRouter();
+
   return (
     <article className="user-card">
         <div className="user-card_avatar">
             <Image src={imgUrl} alt="logo" width={48} height={48} className="rounded-full" />
+
+            <div className="flex-1 text-ellipsis">
+                <h4 className="text-base-semibold text-light-1">{name}</h4>
+                <p className="text-small-medium text-gray-1">@{username}</p>
+            </div>
         </div>
+
+        <Button className="user-card_btn" onClick={() => router.push(`/profile/${id}`)}>
+            View
+        </Button>
     </article>
   )
 }
