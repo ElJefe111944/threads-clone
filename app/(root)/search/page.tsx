cd73a852bd57e8e256883e6f0c88d9c878113cc1
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-09-01 08:11:29 +0100

    fetchUsers action called inside the search component

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import ThreadsTab from "@/components/shared/ThreadsTab";
 import { currentUser } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
-import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 
 
 const page = async () => {
@@ -17,11 +17,34 @@ const page = async () => {
     if (!userInfo?.onboarded) redirect('/onboarding');
 
     // fetch users
-    
+    const result = await fetchUsers({
+        userId: user.id,
+        searchString: '',
+        pageNumber: 1,
+        pageSize: 25,
+    });
+
+
 
     return (
         <section>
             <h1 className="head-text mb-10">Search</h1>
+
+            {/* search bar */}
+
+            <div className="mt-14 flex flex-col gap-9">
+                {result.users.length === 0 ? (
+                    <p className="no-result">No users</p>
+                ) : (
+                    <>
+                        {result.users.map((user) => (
+                            <UserCard 
+                                
+                            />
+                        ))}
+                    </>
+                )}
+            </div>
         </section>
     )
 }
