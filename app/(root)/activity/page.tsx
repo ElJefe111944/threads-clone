90aa5e7184e760400f6643a9fa43ccbbcd198b17
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-09-03 15:59:12 +0100

    Activity page links, images and styling added

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,6 +1,8 @@
 import { currentUser } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import Link from "next/link";
+import Image from "next/image";
 
 
 const page = async () => {
@@ -18,6 +20,27 @@ const page = async () => {
   return (
     <section>
       <h1 className="head-text mb-10">Activity</h1>
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
+          <>
+            {activity.map((activity) => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article className="activity-card">
+                  <Image 
+                    src={activity.author.image} 
+                    alt="Profile Picture"  
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet</p>
+        )}
+      </section>
     </section>
   )
 }
