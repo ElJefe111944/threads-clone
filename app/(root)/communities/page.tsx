dc2ba14fe3798f363de3cca695e1c5fc65e8c883
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-09-11 12:08:19 +0100

    Community card created and styled

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,11 +1,57 @@
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchCommunities } from "@/lib/actions/community.actions";
 
 
 const page = async () => {
+ 
+    const user = await currentUser();
+
+    if (!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    if (!userInfo?.onboarded) redirect('/onboarding');
+
+    // fetch communities
+    const result = await fetchCommunities({
+        searchString: '',
+        pageNumber: 1,
+        pageSize: 25,
+    });
+
+
+
     return (
-      <section>
-          <h1 className="head-text mb-10">Communities</h1>
-      </section>
+        <section>
+            <h1 className="head-text mb-10">Search</h1>
+
+            {/* search bar */}
+
+            <div className="mt-14 flex flex-col gap-9">
+                {result.communities.length === 0 ? (
+                    <p className="no-result">No Communities</p>
+                ) : (
+                    <>
+                        {result.communities.map((community) => (
+                            <UserCard 
+                                key={community.id}
+                                id={community.id}
+                                name={community.name}
+                                username={community.username}
+                                imgUrl={community.image}
+                                personType='Community'
+                            />
+                        ))}
+                    </>
+                )}
+            </div>
+        </section>
     )
-  }
-  
-  export default page
+}
+
+export default page
