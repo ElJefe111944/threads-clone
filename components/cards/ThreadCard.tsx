241bd93854d9287a70b466a1a434b47211c04a91
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-09-12 14:03:53 +0100

    Comment replies added to threadCard

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -27,7 +27,7 @@ interface Props {
     isComment?: boolean,
 }
 
-export default function ThreadCard({ 
+export default function ThreadCard({
     id,
     currentUserId,
     parentId,
@@ -37,49 +37,48 @@ export default function ThreadCard({
     createdAt,
     comments,
     isComment,
- }: Props) {
+}: Props) {
 
 
 
-  return (
-      <article className={`flex w-full flex-col rounded-xl ${isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'}`}>
-          <div className="flex items-start justify-between">
-              <div className="flex w-full flex-1 flex-row gap-4">
-                  <div className="flex flex-col items-center">
-                      <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
-                          <Image src={author.image} alt={author.name} fill className="cursor-pointer rounded-full" />
-                      </Link>
-                      <div className="thread-card_bar" />
-                  </div>
-                  <div className="flex w-full flex-col">
-                      <Link className="w-fit" href={`/profile/${author.id}`}><h4 className="cursor-pointer text-base-semibold text-light-1">{author.name}</h4></Link>
-                      <p className="mt-2 text-small-regular text-light-2">{content}</p>
-                      <div className={`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
-                          <div className="flex gap-3.5">
-                              <Image src='/assets/heart-gray.svg' alt="heart" width={24} height={24} className="cursor-pointer object-contain" />
-                              <Link href={`/thread/${id}`}>
-                                  <Image src='/assets/reply.svg' alt="reply" width={24} height={24} className="cursor-pointer object-contain" />
-                              </Link>
-                              <Image src='/assets/repost.svg' alt="repost" width={24} height={24} className="cursor-pointer object-contain" />
-                              <Image src='/assets/share.svg' alt="share" width={24} height={24} className="cursor-pointer object-contain" />
-                          </div>
+    return (
+        <article className={`flex w-full flex-col rounded-xl ${isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'}`}>
+            <div className="flex items-start justify-between">
+                <div className="flex w-full flex-1 flex-row gap-4">
+                    <div className="flex flex-col items-center">
+                        <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+                            <Image src={author.image} alt={author.name} fill className="cursor-pointer rounded-full" />
+                        </Link>
+                        <div className="thread-card_bar" />
+                    </div>
+                    <div className="flex w-full flex-col">
+                        <Link className="w-fit" href={`/profile/${author.id}`}><h4 className="cursor-pointer text-base-semibold text-light-1">{author.name}</h4></Link>
+                        <p className="mt-2 text-small-regular text-light-2">{content}</p>
+                        <div className={`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
+                            <div className="flex gap-3.5">
+                                <Image src='/assets/heart-gray.svg' alt="heart" width={24} height={24} className="cursor-pointer object-contain" />
+                                <Link href={`/thread/${id}`}>
+                                    <Image src='/assets/reply.svg' alt="reply" width={24} height={24} className="cursor-pointer object-contain" />
+                                </Link>
+                                <Image src='/assets/repost.svg' alt="repost" width={24} height={24} className="cursor-pointer object-contain" />
+                                <Image src='/assets/share.svg' alt="share" width={24} height={24} className="cursor-pointer object-contain" />
+                            </div>
 
-                          {isComment && comments.length > 0 && (
-                            <Link href={`/thread/${id}`}>
-                                <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
-                            </Link>
-                          )}
-                      </div>
-                  </div>
-              </div>
-              {/* delete thread  */}
-              {/* show comment logos */}
-
-          </div>
-          {!isComment && comments.length > 0 && (
-                <div className="ml-1 mt-3 flex items-center gap-2"> 
-                    {comments.slice(0,2).map((comment,index) => (
-                        <Image 
+                            {isComment && comments.length > 0 && (
+                                <Link href={`/thread/${id}`}>
+                                    <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
+                                </Link>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            {/* delete thread  */}
+            </div>
+             {/* show comment logos */}
+            {!isComment && comments.length > 0 && (
+                <div className="ml-1 mt-3 flex items-center gap-2">
+                    {comments.slice(0, 2).map((comment, index) => (
+                        <Image
                             key={index}
                             src={comment.author.image}
                             alt={`user_${index}`}
@@ -88,14 +87,20 @@ export default function ThreadCard({
                             className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
                         />
                     ))}
+                                <Link href={`/thread/${id}`}>
+                <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+                </p>
+            </Link>   
                 </div>
-              )}
-          {!isComment && community && (
+            )}       
+
+            {!isComment && community && (
                 <Link href={`/communities/${community.id}`} className="mt-5 flex items-center">
                     <p className="text-gray-1 text-subtle-medium">
                         {formatDateString(createdAt)} - {community.name} Community
                     </p>
-                    <Image 
+                    <Image
                         src={community.image}
                         width={14}
                         height={14}
@@ -103,7 +108,7 @@ export default function ThreadCard({
                         className="ml-1 rounded-full object-cover"
                     />
                 </Link>
-              )}
-      </article>
-  );
+            )}
+        </article>
+    );
 }
